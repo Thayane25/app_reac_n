47b5de7a5e4fe12a9f569e25523b677ee3072e41
Thayane Lima <130837321+Thayane25@users.noreply.github.com>
2024-08-17 01:04:03 +0000

    Aula 2

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,52 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
+const produtos = [
+  {id: 1, nome: "Coca-cola", preco: 5.5},
+  {id: 2, nome: "Pepsi", preco: 5.5},
+  {id: 3, nome: "Fanta", preco: 5.5},
+  {id: 4, nome: "Guaraná", preco: 5.5}
+]
+var contador = 0;
+
 
 export default function Index() {
+  let [contador, setcontador]= useState(0);
   return (
     <View
       style={estilo.container}
     >
-      <Text style={estilo.text}>HELLO WORLD</Text>
+      {produtos.map((p)=> (
+        <View>
+        <Text style={estilo.titulo}  >{p.nome}</Text>
+        <Text>{p.preco}</Text>
+       </View>
+
+    ))}
     </View>
+
+    
+  
   );
 }
-const estilo = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FC8EAC",
-  },
-  text: {
-    color:"#000000"
-  }
-})
+
+  const estilo = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "flex-start",
+      backgroundColor: "#FC8EAC",
+      padding: 20,
+    },
+    text: {
+      color:"#000000"
+    },
+    titulo:{
+      color:"#000000",
+      fontSize: 30,
+      textAlign: "left",
+      fontWeight: "bold", 
+      
+    }
+  })
+  
+
